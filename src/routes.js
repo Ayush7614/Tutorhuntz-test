f48321f23464013d4241118221e00f62fd897799
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2022-01-30 20:42:15 +0530

    update codebase

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,6 @@ import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import DashboardApp from './pages/DashboardApp';
-import Products from './pages/Products';
-import Blog from './pages/Blog';
 import User from './pages/User';
 import NotFound from './pages/Page404';
 
@@ -21,9 +19,7 @@ export default function Router() {
       children: [
         { element: <Navigate to="/dashboard/app" replace /> },
         { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> }
+        { path: 'user', element: <User /> }
       ]
     },
     {
