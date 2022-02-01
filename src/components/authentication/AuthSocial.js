4a06765e1dbec0ef615b8b78772317f7266c575b
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2022-02-01 19:59:10 +0530

    added codebase

--- src/components/authentication/AuthSocial.js
+++ src/components/authentication/AuthSocial.js
@@ -12,7 +12,7 @@ export default function AuthSocial() {
     <>
       <Stack direction="row" spacing={2}>
         <Button fullWidth size="large" color="inherit" variant="outlined">
-          <Icon icon={googleFill} color="#DF3E30" height={24} />
+          <Icon icon={googleFill}  color="#DF3E30" height={24} />
         </Button>
 
         <Button fullWidth size="large" color="inherit" variant="outlined">
